9ebc8f3565d14e46b8bd7071a19fac91e04e08bf
Edwin Lu <139795686+ewlu@users.noreply.github.com>
2023-09-19 17:48:25 -0700

    Gate patchworks api (#165)
    * added warn/error/pass in gate

---
+++ scripts/download_single_artifact.py
@@ -0,0 +1,58 @@
+import argparse
+from github import Auth, Github
+from download_artifacts import download_artifact
+
+
+def parse_arguments():
+    """ Parse command line arguments """
+    parser = argparse.ArgumentParser(description="Download single log artifact")
+    parser.add_argument(
+        "-name",
+        required=True,
+        type=str,
+        help="Name of the artifact",
+    )
+    parser.add_argument(
+        "-token",
+        required=True,
+        type=str,
+        help="Github access token",
+    )
+    parser.add_argument(
+        "-outdir",
+        required=True,
+        type=str,
+        help="output dir to put downloaded file"
+    )
+    parser.add_argument(
+        "-repo",
+        default="patrick-rivos/riscv-gnu-toolchain",
+        type=str,
+        help="which repo to pull from"
+    )
+    return parser.parse_args()
+
+
+def download_artifact_with_name(artifact_name: str, token: str, outdir: str, repo: str):
+    """
+    Download the artifact with a given name into ./logs.
+    """
+    auth = Auth.Token(token)
+    g = Github(auth=auth)
+
+    repo = g.get_repo(repo)
+
+    artifacts = repo.get_artifacts(artifact_name).get_page(0)
+    if len(artifacts) != 0:
+        download_artifact(artifact_name, str(artifacts[0].id), token, outdir)
+    else:
+        raise ValueError(f"Failed to find artifact for {artifact_name}")
+
+
+def main():
+    args = parse_arguments()
+    download_artifact_with_name(args.name, args.token, args.outdir, args.repo)
+
+
+if __name__ == "__main__":
+    main()
