cb33d60cc53bcc0f6eb9f33f58843cc0746ded3c
Edwin Lu <139795686+ewlu@users.noreply.github.com>
2023-09-21 12:05:00 -0700

    Add patchworks reporting throughout pipeline (#177)

--- scripts/post_check_to_patchworks.py
+++ scripts/post_check_to_patchworks.py
@@ -20,6 +20,29 @@ def parse_arguments():
         help="Check type (linter, build, etc)",
     )
     parser.add_argument(
+        "-state",
+        "--state",
+        metavar="<string>",
+        default="pending"
+        type=str,
+        help="check state",
+    )
+    parser.add_argument(
+        "-context",
+        "--context",
+        required=True,
+        metavar="<string>",
+        type=str,
+        help="What test we reporting for",
+    )
+    parser.add_argument(
+        "-rid",
+        "--run-id",
+        metavar="<string>",
+        type=str,
+        help="run id",
+    )
+    parser.add_argument(
         "-iid",
         "--issue-id",
         metavar="<string>",
@@ -28,13 +51,17 @@ def parse_arguments():
     )
     return parser.parse_args()
 
-def send(patch_id: str, desc: str, issue: str):
-    target_url = f"https://github.com/ewlu/riscv-gnu-toolchain/issues/{issue}"
+def send(patch_id: str, desc: str, issue: str, rid: str, state: str, context: str):
+    target_url = None
+    if issue == "":
+        target_url = f"https://github.com/ewlu/riscv-gnu-toolchain/actions/runs/{rid}"
+    else:
+        target_url = f"https://github.com/ewlu/riscv-gnu-toolchain/issues/{issue}"
     url = f"https://patchwork.sourceware.org/api/1.3/patches/{patch_id}/checks/"
     params = {
-        "state": "pending",
+        "state": state,
         "target_url": target_url,
-        "context": "toolchain-ci-rivos",
+        "context": f"toolchain-ci-rivos/{context}",
         "description": desc
     }
 
@@ -46,7 +73,7 @@ def send(patch_id: str, desc: str, issue: str):
 
 def main():
     args = parse_arguments()
-    send(args.patch_id, args.description, args.issue_id)
+    send(args.patch_id, args.description, args.issue_id, args.run_id, args.state, args.context)
 
 if __name__ == "__main__":
     main()
