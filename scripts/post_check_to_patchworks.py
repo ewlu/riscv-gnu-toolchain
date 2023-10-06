a673e9bef5e3ef20ddb46e49f88498f49227ff86
Edwin Lu <ewlu@rivosinc.com>
2023-10-06 11:22:00 -0700

    gate posts to comments only

--- scripts/post_check_to_patchworks.py
+++ scripts/post_check_to_patchworks.py
@@ -1,5 +1,6 @@
 import requests
 import argparse
+from typing import Dict
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Send api response")
@@ -18,6 +19,14 @@ def parse_arguments():
         required=True,
         type=str,
         help="Check type (linter, build, etc)",
+    )
+    parser.add_argument(
+        "-token",
+        "--token",
+        metavar="<string>",
+        required=True,
+        type=str,
+        help="Patchworks api token",
     )
     parser.add_argument(
         "-state",
@@ -49,31 +58,49 @@ def parse_arguments():
         type=str,
         help="issue number",
     )
+    parser.add_argument(
+        "-comment",
+        action="store_true",
+        help="is comment",
+    )
     return parser.parse_args()
 
-def send(patch_id: str, desc: str, issue: str, rid: str, state: str, context: str):
+def create_data(desc: str, issue: str, rid: str, state: str, context: str):
     target_url = None
-    if issue == "":
+    if issue is None or issue == "":
         target_url = f"https://github.com/ewlu/riscv-gnu-toolchain/actions/runs/{rid}"
     else:
         target_url = f"https://github.com/ewlu/riscv-gnu-toolchain/issues/{issue}"
-    url = f"https://patchwork.sourceware.org/api/1.3/patches/{patch_id}/checks/"
-    params = {
+    data = {
         "state": state,
         "target_url": target_url,
-        "context": f"toolchain-ci-rivos/{context}",
+        "context": f"toolchain-ci-rivos-{context}",
         "description": desc
     }
+    return data
 
-    # x = requests.post(url, json=params)
-    # print(x.text)
-    print(f"target: {target_url}")
-    print(f"post url: {url}")
-    print(f"params: {params}")
+def create_headers(token: str):
+    headers = { "Authorization": f"Token {token}" }
+    return headers
+    
+
+def send(patch_id: str, data: Dict[str, str], headers: Dict[str, str]):
+    url = f"https://patchwork.sourceware.org/api/1.3/patches/{patch_id}/checks/"
+
+    print("is comment. making post request")
+    
+    response = requests.post(url, data=data, headers=headers)
+    print(response.status_code)
+    print(response.text)
 
 def main():
     args = parse_arguments()
-    send(args.patch_id, args.description, args.issue_id, args.run_id, args.state, args.context)
+    data = create_data(args.description, args.issue_id, args.run_id, args.state, args.context)
+    headers = create_headers(args.token)
+    print(f"data: {data}")
+    print(args.comment)
+    if args.comment:
+        send(args.patch_id, data, headers)
 
 if __name__ == "__main__":
     main()
